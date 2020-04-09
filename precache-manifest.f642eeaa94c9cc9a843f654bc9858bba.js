b17d8ab88a4d42e32cd2372dd7af1cc10b21868c
Mocskos Mihaly <mocskos.misi@gmail.com>
2020-04-09 16:59:00 +0200

    Updates

---
+++ precache-manifest.f642eeaa94c9cc9a843f654bc9858bba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413b6f3c64bffaf27e9739882480b2fa",
+    "url": "/covid19reactproject-/index.html"
+  },
+  {
+    "revision": "b060b3fb48fb2fc63a12",
+    "url": "/covid19reactproject-/static/css/main.8b9ea47a.chunk.css"
+  },
+  {
+    "revision": "ceb3ab52eaa680bcfd26",
+    "url": "/covid19reactproject-/static/js/2.7802374d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19reactproject-/static/js/2.7802374d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b060b3fb48fb2fc63a12",
+    "url": "/covid19reactproject-/static/js/main.14a81248.chunk.js"
+  },
+  {
+    "revision": "a153f4b039f707e004a5",
+    "url": "/covid19reactproject-/static/js/runtime-main.72e9b689.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19reactproject-/static/media/image.d7265326.png"
+  }
+]);
